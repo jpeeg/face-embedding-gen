7618255e364232e8927ad281655af595da3480fb
jpeeg <32647093+jpeeg@users.noreply.github.com>
2022-04-21 23:12:43 +0100

    Create ui.py

---
+++ ui.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import requests
+import numpy as np
+from PIL import Image
+import json
+import io
+
+API_ENDPOINT = "https://face-embedding-generator.herokuapp.com/predict"
+
+# Create the header page content
+st.title("Face Similarity Service")
+st.markdown("### Calculate a similarity score between two faces",
+            unsafe_allow_html=True)
+
+st.text("Please upload an image of each of the people you want to compare")
+
+
+def predict(img):
+    bytes_image = img.getvalue()
+    np_array = np.array(Image.open(io.BytesIO(bytes_image)))
+
+
+    # Send the image to the API
+    response = requests.post(
+        API_ENDPOINT, 
+        headers={'Content-type': 'application/json'}, 
+        data=json.dumps(np_array.tolist())
+    )
+
+    if response.status_code == 200:
+        return response.text
+    else:
+        raise Exception("Status: {}".format(response.status_code))
+
+
+def main():
+    img1_file = st.file_uploader("Upload an image", type=["jpg", "png"])
+    img2_file = st.file_uploader("Upload an image", type=["jpg", "png"])
+
+    if img1_file is not None and img2_file is not None:
+        with st.spinner("Calculating Similarity Score..."):
+            img1_embedding = predict(img1_file)
+            img2_embedding = predict(img2_file)
+            # print(type(img1_embedding), type(img2_embedding))
+            st.success(f"Your similarity score is {type(img1_embedding)}, {type(img2_embedding)}")
+
+    # camera_input = st.camera_input("Or take a picture")
+    # if camera_input is not None:
+    #     with st.spinner("Predicting..."):
+    #         prediction = float(predict(camera_input).strip("[").strip("]"))
+    #         st.success(f"Your pet's cuteness score is {prediction:.3f}")
+
+
+if __name__ == "__main__":
+    main()
